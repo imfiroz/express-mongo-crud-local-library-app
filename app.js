60633c2e23c7262f5339e23f57ff791999ab0b72
manerao-pritam <manerao.pritam@gmail.com>
2018-10-26 22:45:31 +0100

    modified:   app.js 	modified:   controllers/authorController.js 	modified:   controllers/bookController.js 	modified:   controllers/bookinstanceController.js 	new file:   views/author_delete.pug 	modified:   views/author_detail.pug 	modified:   views/book_detail.pug 	modified:   views/book_form.pug 	new file:   views/bookinstance_form.pug

--- app.js
+++ app.js
@@ -1,66 +1,66 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var favicon = require('serve-favicon')
+let createError 	= require('http-errors')
+let express 		= require('express')
+let path 			= require('path')
+let cookieParser 	= require('cookie-parser')
+let logger 			= require('morgan')
+let favicon 		= require('serve-favicon')
 
 // DB: Start
 
 // Import the mongoose module
-var mongoose = require('mongoose');
+let mongoose = require('mongoose')
 
 // Set up default mongoose connection
-var mongoDB = 'mongodb://127.0.0.1/Movies';
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+let mongoDB = 'mongodb://127.0.0.1/Movies'
+mongoose.connect(mongoDB, {useNewUrlParser: true})
 
 // Get mongoose to use the global promise library
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
 // Get the default connection
-var db = mongoose.connection;
+let db = mongoose.connection
 
 // Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error'));
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
 // DB: End
 
-var indexRouter     = require('./routes/index');
-var usersRouter     = require('./routes/users');
-var catalogRouter   = require('./routes/catalog');  //Import routes for "catalog" area of site
+let indexRouter     = require('./routes/index')
+let usersRouter     = require('./routes/users')
+let catalogRouter   = require('./routes/catalog')  //Import routes for "catalog" area of site
 
-var app = express();
+let app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
 // uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/catalog', catalogRouter); // Add catalog routes to middleware chain.
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/catalog', catalogRouter) // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
-});
+	next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+	// render the error page
+	res.status(err.status || 500)
+	res.render('error')
+})
 
-module.exports = app;
+module.exports = app
--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,66 +1,114 @@
-var BookInstance = require('../models/bookinstance');
+let BookInstance    = require('../models/bookinstance')
+let Book            = require('../models/book')
 
-// // Display list of all BookInstances
-// exports.bookinstance_list = function(req, res) {
-//     res.send('Not implemented: BookInstance List');
-// };
+const {body, validationResult} = require('express-validator/check')
+const {sanitizeBody} = require('express-validator/filter')
 
 // Display list of all BookInstances
 exports.bookinstance_list = function(req, res, next) {
     BookInstance.find()
         .populate('book')
         .exec(function(err, list_bookinstances) {
-           if(err)  { return next(err); }
-            res.render('bookinstance_list', {title: 'Book Instance List', bookinstance_list: list_bookinstances});
-        });
-};
+           if(err)  { return next(err) }
+            res.render('bookinstance_list', {title: 'Book Instance List', bookinstance_list: list_bookinstances})
+        })
+}
 
 // Display detail page for a specific BookInstance
 exports.bookinstance_detail = function(req, res) {
-    // _id = req.params.id;
+    // _id = req.params.id
 
     BookInstance.findById(req.params.id).populate('book')
         .exec(function(err, bookinstance) {
-            if(err) {return next(er);}
+            if(err) {return next(er)}
 
             // No results
             if(bookinstance == null) {
-                var err     = new Error('Book copy not found');
-                err.status  = 404;
-                return next(err);
+                let err     = new Error('Book copy not found')
+                err.status  = 404
+                return next(err)
             }
 
             // Successful, so render
-            res.render('bookinstance_detail', {title: 'Book:', bookinstance: bookinstance});
-        });
-};
+            res.render('bookinstance_detail', {title: 'Book:', bookinstance: bookinstance})
+        })
+}
 
 // Display BookInstances create form on GET
-exports.bookinstance_create_get = function(req, res) {
-    res.send('Not implemented: BookInstance create GET');
-};
+exports.bookinstance_create_get = function(req, res, next) {
+    Book.find({}, 'title').exec(function(err, books) {
+        if(err) {return next(err)}
+
+        // Successful, so render
+        res.render('bookinstance_form', {title: 'Create Book Instance', book_list: books})
+    })
+}
 
 // Handle BookInstances create on POST
-exports.bookinstance_create_post = function(req, res) {
-    res.send('Not implemented: BookInstance create POST');
-};
+exports.bookinstance_create_post = [
+    // Validate fields
+    body('book', 'Book must be specified').isLength({min: 1}).trim(),
+    body('imprint', 'Imprint must be specified').isLength({min: 1}).trim(),
+    body('due_back', 'Invalid date').optional({checkFalsy: true}).isISO8601(),
+
+    // Sanitize fields
+    sanitizeBody('book').trim().escape(),
+    sanitizeBody('imprint').trim().escape(),
+    sanitizeBody('status').trim().escape(),
+    sanitizeBody('due_back').trim().escape(),
+
+    // Process request after validation and sanitization
+    (req, res, next) => {
+
+        // Extract the validation errors from a request
+        const errors = validationResult(req)
+
+        // Create a BookInstance object with escaped and trimmed data
+        let bookinstance = new BookInstance({
+            book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back
+        })
+
+        if(!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values and error message
+            Book.find({}, 'title').exec(function(err, books) {
+                if(err) {return next(err)}
+
+                // Successful, so render
+                res.render('bookinstance_form', {title: 'Create Book Instance', book_list: books, selected_book: bookinstance.book._id, errors: errors.array(), bookinstance: bookinstance})
+            })
+
+            return
+        } else {
+            // Data from form is valid
+            bookinstance.save(function(err) {
+                if(err) {return next(err)}
+
+                // Successful, so render
+                res.redirect(bookinstance.url)
+            })
+        }
+    }
+]
 
 // Display BookInstances delete form on GET
 exports.bookinstance_delete_get = function(req, res) {
-    res.send('Not implemented: BookInstance delete GET');
-};
+    res.send('Not implemented: BookInstance delete GET')
+}
 
 // Handle BookInstances delete on POST
 exports.bookinstance_delete_post = function(req, res) {
-    res.send('Not implemented: BookInstance delete POST');
-};
+    res.send('Not implemented: BookInstance delete POST')
+}
 
 // Display BookInstances update form on GET
 exports.bookinstance_update_get = function(req, res) {
-    res.send('Not implemented: BookInstance update GET');
-};
+    res.send('Not implemented: BookInstance update GET')
+}
 
 // Handle BookInstances update on POST
 exports.bookinstance_update_post = function(req, res) {
-    res.send('Not implemented: BookInstance update POST');
-};
+    res.send('Not implemented: BookInstance update POST')
+}
